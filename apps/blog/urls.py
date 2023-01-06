944791e00c70dc53c61b15bc0583c19df7097b9e
rbenprieto <rbenprieto@outlook.com>
2023-01-06 16:06:39 -0500

    statics and templates

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
+from .views import (
+    home,
+    generales,
+    programacion,
+    contacto
+)
 
 urlpatterns = [
-    
+    path('', home, name='index'),
+    path('generales/', generales, name='generales'),
+    path('programacion/', programacion, name='programacion'),
+    path('contacto/', contacto, name='contacto'),
+
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import (
+    Post,
+    Categoria,
+    Autor
+)
 
-# Create your views here.
+def home(request):
+    post = Post.objects.filter(estado=True)
+    print(post)
+    return render(request, 'index.html')
+
+
+def generales(request):
+    return render(request, 'generales.html')
+
+def programacion(request):
+    return render(request, 'programacion.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
