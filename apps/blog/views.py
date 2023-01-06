b1eb3b128a783c2c903728f41b691e9cf01a04e9
rbenprieto <rbenprieto@outlook.com>
2023-01-06 16:34:27 -0500

    posts in templates

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -7,15 +7,17 @@ from .models import (
 
 def home(request):
     post = Post.objects.filter(estado=True)
-    print(post)
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'posts': post})
 
 
 def generales(request):
-    return render(request, 'generales.html')
+    post = Post.objects.filter(estado=True, categoria= Categoria.objects.filter(nombre='Generales').first())
+    return render(request, 'generales.html', {'posts': post})
 
 def programacion(request):
-    return render(request, 'programacion.html')
+    post = Post.objects.filter(estado=True, categoria= Categoria.objects.filter(nombre='Programación').first())
+    return render(request, 'programacion.html', {'posts': post})
 
 def contacto(request):
-    return render(request, 'contacto.html')
+    post = Post.objects.filter(estado=True, categoria= Categoria.objects.filter(nombre='Contacto').first())
+    return render(request, 'contacto.html', {'posts': post})
