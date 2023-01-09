47a2b71223e20b3f6239fd8a60cac1f01868d5dd
rbenprieto <rbenprieto@outlook.com>
2023-01-08 22:07:13 -0500

    admin update

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -34,6 +34,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #THIRD
+    'jazzmin',
+
+    #NATIVE
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
